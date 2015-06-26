b5eea0d25d1016ecbff19ed48cd95bbc3e24a2a9
AntoPonto <a.pontorieri@wingtronics.it>
2015-06-26 17:50:34 +0200

    llk,lkl
    ll

--- NHibernateTest/NHibernateConfigurator.cs
+++ NHibernateTest/NHibernateConfigurator.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
-
+45484545
 namespace NHibernateTest
 {
     public partial class NHibernateConfigurator
